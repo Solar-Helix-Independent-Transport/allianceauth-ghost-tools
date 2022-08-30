317178109ccef1d384eb9c05610ef20354c18f92
Aaron Kable <aaronkable@gmail.com>
2022-08-30 18:57:24 +0800

    API url correction

--- frontend/src/apis/Dashboard.js
+++ frontend/src/apis/Dashboard.js
@@ -2,7 +2,7 @@ import axios from "axios";
 axios.defaults.xsrfHeaderName = "X-CSRFToken";
 
 export async function loadDash() {
-  const api = await axios.get(`ghosts/api/ghost/list`);
+  const api = await axios.get(`/ghosts/api/ghost/list`);
   console.log(`get structure list in api`);
   console.log(api);
   return api.data;
