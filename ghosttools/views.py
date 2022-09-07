a25b6c7f4edea1d9be8ae27a14a2d5bcb6d4ad72
Aaron Kable <aaronkable@gmail.com>
2022-09-07 21:20:44 +0800

    perms

--- ghosttools/views.py
+++ ghosttools/views.py
@@ -8,20 +8,20 @@ from .models import GhostKickToken
 
 
 @login_required
-@permission_required('ghosttools.view_dash')
+@permission_required('ghosttools.access_ghost_tools')
 def react_bootstrap(request):
     return render(request, 'ghosttools/react_base.html', context={"version": __version__})
 
 
 @login_required
-@permission_required('ghosttools.view_dash')
+@permission_required('ghosttools.access_ghost_tools')
 @token_required(scopes=['esi-corporations.track_members.v1', 'esi-characters.read_corporation_roles.v1'])
 def ghost_corp_add(request, token):
     return redirect('ghosttools:view')
 
 
 @login_required
-@permission_required('ghosttools.view_dash')
+@permission_required('ghosttools.access_ghost_tools')
 @token_required(['esi-ui.open_window.v1', 'esi-location.read_online.v1'])
 def ghost_setkick_character(request, token):
     if token:
